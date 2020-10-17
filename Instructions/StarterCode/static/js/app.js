f9a039ae95e4614a6cb23e5667511bf07dca1d38
unknown <ngashbaugh@gmail.com>
2020-10-17 09:20:04 -0700

    Comments added

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -4,14 +4,15 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody")
 
-
+// present the table when the web page is loaded 
 table(tableData)
 
 
-
-// Runs table creation 
+// Runs table creation or filter when called 
 function table(Tdata) {
+    // Clears the HTML Table
     tbody.html("")
+    // Go through each row of data and create a table with that data in HTML
     Tdata.forEach(function (line) {
         var row = {}
         row = tbody.append('tr');
@@ -21,10 +22,12 @@ function table(Tdata) {
     }) 
 };
 
+// create the default selection for the filter 
+var type = "date"
 
 // Runs Date filter
 function runbuttonD () {
-    
+    // Prevent the page from refreshing 
     d3.event.preventDefault();
 
     // Select the input element and get the raw HTML node
@@ -34,87 +37,44 @@ function runbuttonD () {
     var inputValue = inputElement.property("value");
     console.log(inputValue)
     var newData = {}
-    
+    // If statements that choose the type of filteing wanted
     if(type.node().value === "date") {
         newData = tableData.filter(function(frow){
-            // console.log(frow.datetime)
-            // console.log(inputValue)
                 return frow.datetime == inputValue
     })};
+
     if(type.node().value === "city") {
         newData = tableData.filter(function(frow){
-            console.log(frow.city)
-            console.log(inputValue)
-
                 return frow.city == inputValue
     })};
     if(type.node().value === "state") {
         newData = tableData.filter(function(frow){
-            console.log(frow.state)
-            console.log(inputValue)
                 return frow.state == inputValue
     })};
     if(type.node().value === "country") {
         newData = tableData.filter(function(frow){
-            console.log(frow.datetime)
-            console.log(inputValue)
                 return frow.country == inputValue
     })};
     if(type.node().value === "shape") {
         newData = tableData.filter(function(frow){
-            console.log(frow.datetime)
-            console.log(inputValue)
-
                 return frow.shape == inputValue
     })};
-
-
-
-    console.log(newData)
+    // Return the filtered data and run it through the HTML table creater 
     return table(newData)
 };
 
-
+// Change the filter type 
 function change () {
     var type = d3.select('#filterSelect').property('value');
     console.log(type)
     return type
 };
 
-
+// when the dropdown is changed this trigers the function 
 type = d3.selectAll('#filterSelect').on('change' , change )
 
-// Activates Date filter
+// grabs location to acticate date filter.
 var buttonD = d3.select('#filter-btn-Date')
 
+// Activates date filter
 buttonD.on("click", runbuttonD);
-
-
-// // Runs City Filter
-// function runbuttonC () {
-    
-//     d3.event.preventDefault();
-
-//     // Select the input element and get the raw HTML node
-//     var inputElementC = d3.select("#city");
-
-//     // Get the value property of the input element
-//     var inputValueC = inputElementC.property("value");
-
-
-//     var newData = tableData.filter(function(frow){
-//             return frow.city == inputValueC
-//     })
-
-//     console.log(newData)
-//     return table(newData)
-
-
-// };
-
-
-// var buttonC = d3.select('#filter-btn-City')
-
-// buttonC.on("click", runbuttonC);
-
-
